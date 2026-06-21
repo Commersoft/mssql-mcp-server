42b55d0c65049b9742b9655a83b745cca62d567b
jmk <jacek.mikucki@certusoft.pl>
2026-06-21 12:34:58 +0200

    chore: remove temporary debug query file

--- _q.sql
+++
@@ -1,5 +0,0 @@
-select csAIAgentsId, csAIAgentsG, name, left(role,100) as role_preview, csCompaniesId, isDefault, availableForEveryone
-from dbo.csAIAgents
-where name like N'%Radek%'
-   or name like N'%radek%'
-order by csAIAgentsId
